b921e5ed4233c5be96036e7a07e58069f3f12934
Jia Xuan <jiaxuanling2004@gmail.com>
2026-09-22 19:25:45 +0800

    Added Admin Portal

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -13,9 +13,28 @@ def registration():
     return render_template('registration.html')
 
 
+@app.route('/registration/success')
+def registration_success():
+    return render_template('registration-success.html')
+
+
 @app.route('/submission')
 def submission():
-    return render_template('submission.html')
+    return redirect(url_for('student_portal') + '#project')
+
+@app.route('/student')
+def student_portal():
+    return render_template('student.html')
+
+
+@app.route('/admin')
+def admin_portal():
+    return render_template('admin.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
